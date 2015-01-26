b4c34ba123da52e42495d8a579e611e558125589
tanem <tane.morgan@gmail.com>
2015-01-26 14:07:16 +1300

    Add contenthash transform test for custom options

--- test/transforms/contenthash.test.js
+++ test/transforms/contenthash.test.js
@@ -24,3 +24,25 @@ test('should append the content hash to the filename', function(t){
       callback();
     }));
 });
+
+test('should specification of the algorithm and encoding', function(t){
+  t.plan(1);
+  
+  var filePath = path.join(os.tmpdir(), 'a.txt');
+  fs.writeFileSync(filePath, 'Hello from a');
+
+  var rs = new Readable({ objectMode: true });  
+  rs.push({ oldPath: filePath, newPath: '' });
+  rs.push(null);
+  rs.pipe(contenthash({
+    algorithm: 'sha1',
+    encoding: 'hex'
+    }))
+    .pipe(through.obj(function(chunk, encoding, callback){
+      t.deepEqual(chunk, {
+        oldPath: filePath,
+        newPath: path.join(os.tmpdir(), 'a.f24413be.txt')
+      });
+      callback();
+    }));
+});
